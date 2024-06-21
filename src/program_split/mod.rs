2aa1ebe0e3ec71bba96ab56c5898cb8080b30e31
PayneJoe <joepayne@163.com>
2024-06-21 17:32:52 +0800

    fixed the depth

--- src/program_split/mod.rs
+++ src/program_split/mod.rs
@@ -23,8 +23,10 @@ pub fn demo_script(num: usize) -> Script {
 }
 
 pub fn tap_tree_builder(num_scripts: usize) {
-    let secp = bitcoin::secp256k1::Secp256k1::new();
+    let log2 = (num_scripts as u32).ilog2() as u8;
+    let prev_power_of_2 = 2_usize.pow(log2 as u32);
 
+    let secp = bitcoin::secp256k1::Secp256k1::new();
     // public key with x-coordinate
     let internal_key = UntweakedPublicKey::from(
         bitcoin::secp256k1::PublicKey::from_str(
@@ -37,10 +39,28 @@ pub fn tap_tree_builder(num_scripts: usize) {
     let scripts = (0..num_scripts).map(|i| demo_script(i)).collect::<Vec<_>>();
 
     // add the individual scripts into Taptree (as leaf node)
+    // push the first prev_power_of_2 leaf nodes
     let mut builder = TaprootBuilder::new();
-    for i in 0..num_scripts {
-        builder = builder.add_leaf(0, scripts[i].clone()).unwrap();
+    for i in 0..prev_power_of_2 {
+        builder = builder.add_leaf(log2 + 1, scripts[i].clone()).unwrap();
     }
+    // [None, N1]
+
+    // push the remained leaf nodes except the last one
+    for i in prev_power_of_2..(num_scripts - 1) {
+        let depth = 2 + i - prev_power_of_2;
+        builder = builder.add_leaf(depth as u8, scripts[i].clone()).unwrap();
+    }
+    // [None, N1, a_m, a_{m + 1}, a_{n - 2}]
+
+    // push the last leaf node
+    builder = builder
+        .add_leaf(
+            (num_scripts - prev_power_of_2) as u8,
+            scripts[num_scripts - 1].clone(),
+        )
+        .unwrap();
+    // [N0]
 
     // build tree
     let tree_info = builder.finalize(&secp, internal_key).unwrap();
