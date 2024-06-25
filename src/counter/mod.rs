d238214d07f46d194bb4a7c4514413a37b4d956c
PayneJoe <joepayne@163.com>
2024-06-25 20:04:08 +0800

    construct taptree gadget

--- src/counter/mod.rs
+++ src/counter/mod.rs
@@ -1,3 +1,4 @@
+mod script_taptree;
 mod utils;
 
 use crate::treepp::*;
@@ -289,244 +290,32 @@ pub fn get_tx(info: &CounterUpdateInfo) -> (TxTemplate, u32) {
     (tx_template, randomizer)
 }
 
+/// toy script of plus one on counter
 pub fn get_script_counter_plus_one() -> Script {
     script! {
         { utils::convenant(DUST_AMOUNT) }
 
         OP_FROMALTSTACK OP_FROMALTSTACK
+        // [prev_counter, new_counter]
         OP_1SUB OP_EQUAL
     }
 }
 
-pub fn get_script() -> Script {
-    // Obtain the secp256k1 dummy generator, which would be point R in the signature, as well as
-    // the public key.
-    let secp256k1_generator = SECP256K1_GENERATOR.clone();
-
+/// toy script of plus n on counter
+pub fn get_script_counter_plus_n(n: u32) -> Script {
     script! {
-        // For more information about the construction of the Tap CheckSigVerify Preimage, please
-        // check out the `covenants-gadgets` repository.
+        { utils::convenant(DUST_AMOUNT) }
 
-        { tap_csv_preimage::Step1EpochGadget::default() }
-        { tap_csv_preimage::Step2HashTypeGadget::from_constant(&TapSighashType::AllPlusAnyoneCanPay) }
-        { tap_csv_preimage::Step3VersionGadget::from_constant(&Version::ONE) }
-        { tap_csv_preimage::Step4LockTimeGadget::from_constant_absolute(&LockTime::ZERO) }
-        OP_CAT4
-
-        // current stack body: |1-4|
-
-        // first output: the same script itself
-
-        // get a hint: new balance (8 bytes)
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 8 OP_EQUALVERIFY
-
-        // get a hint: this script's scriptpubkey (34 bytes)
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 34 OP_EQUALVERIFY
-
-        // save a copy to the altstack
-        OP_DUP OP_TOALTSTACK
-
-        OP_PUSHBYTES_1 OP_PUSHBYTES_34
-        OP_SWAP OP_CAT3
-
-        OP_FROMALTSTACK OP_SWAP
-
-        // current stack body: |1-4|, this scriptpubkey, |output1|
-
-        // second output: the data carrier
-
-        // the balance must be DUST_AMOUNT
-        OP_PUSHBYTES_8 OP_PUSHBYTES_74 OP_PUSHBYTES_1 OP_PUSHBYTES_0 OP_PUSHBYTES_0
-        OP_PUSHBYTES_0 OP_PUSHBYTES_0 OP_PUSHBYTES_0 OP_PUSHBYTES_0
-        OP_CAT
-
-        // push the script hash header
-        OP_PUSHBYTES_2 OP_RETURN OP_PUSHBYTES_8
-
-        // get a hint: the actual counter value in Bitcoin integer format (<=4 bytes)
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_1ADD OP_1SUB
-        OP_DUP 0 OP_GREATERTHAN OP_VERIFY
-
-        // save the previous number into the altstack for later use
-        OP_DUP OP_1SUB OP_TOALTSTACK
-
-        // extend the actual counter to 4 bytes
-        { CppInt32Gadget::from_positive_bitcoin_integer() }
-
-        // get a hint: the randomizer for this transaction (4 bytes)
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 4 OP_EQUALVERIFY
-        OP_CAT3
-
-        OP_SHA256
-
-        OP_PUSHBYTES_3 OP_PUSHBYTES_34 OP_PUSHBYTES_0 OP_PUSHBYTES_32
-        OP_SWAP OP_CAT3
-
-        OP_SHA256
-        OP_ROT OP_SWAP OP_CAT2
-
-        OP_FROMALTSTACK OP_SWAP
-
-        { tap_csv_preimage::Step7SpendTypeGadget::from_constant(1, false) } OP_CAT2
-
-        // current stack body: this scriptpubkey, previous counter, |1-7|
-
-        // get a hint: previous tx's txid
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 32 OP_EQUALVERIFY
-
-        // save a copy to altstack
-        OP_DUP OP_TOALTSTACK
-
-        // require the output index be 0
-        { tap_csv_preimage::step8_data_input_part_if_anyonecanpay::step1_outpoint::Step2IndexGadget::from_constant(0) }
-        OP_CAT3
-
-        // get a hint: previous tx's amount
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 8 OP_EQUALVERIFY
-        OP_DUP OP_TOALTSTACK
-        OP_CAT2
-
-        // add the script pub key
-        2 OP_PICK
-        OP_PUSHBYTES_1 OP_PUSHBYTES_34 OP_SWAP
-        OP_CAT3
-
-        // require the input sequence number be 0xffffffff
-        { tap_csv_preimage::step8_data_input_part_if_anyonecanpay::Step4SequenceGadget::from_constant(&Sequence::default()) }
-        OP_CAT2
-
-        OP_FROMALTSTACK OP_SWAP
-        OP_FROMALTSTACK OP_SWAP
-
-        // current stack body: this scriptpubkey, previous counter, previous tx's amount, previous tx's txid, |1-11|
-
-        // get a hint: tap leaf hash
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 32 OP_EQUALVERIFY
-
-        { tap_csv_preimage::step12_ext::Step2KeyVersionGadget::from_constant(0) }
-        { tap_csv_preimage::step12_ext::Step3CodeSepPosGadget::no_code_sep_executed() }
-        OP_CAT4
-
-        // current stack body: this scriptpubkey, previous counter, previous tx's amount, previous tx's txid, this script's checksighash preimage
-        { TaggedHashGadget::from_provided(&HashTag::TapSighash) }
-
-        { secp256k1_generator.clone() }
-        OP_DUP OP_TOALTSTACK
-        OP_DUP OP_TOALTSTACK
-
-        OP_DUP OP_ROT OP_CAT3
-
-        { TaggedHashGadget::from_provided(&HashTag::BIP340Challenge) }
-
-        // get a hint: the sha256 without the last byte
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 31 OP_EQUALVERIFY
-
-        OP_DUP { 1 } OP_CAT
-        OP_ROT OP_EQUALVERIFY
-
-        // current stack body:
-        //   this scriptpubkey, previous counter, previous tx's amount, previous tx's txid,
-        //   prefix
-
-        OP_FROMALTSTACK OP_SWAP
-
-        OP_PUSHBYTES_2 OP_PUSHBYTES_2 OP_RIGHT
-        OP_CAT3
-
-        OP_FROMALTSTACK
-        OP_CHECKSIGVERIFY
-
-        // current stack body:
-        //   this scriptpubkey, previous counter, previous tx's amount, previous tx's txid
-
-        { tx::Step1VersionGadget::from_constant(&Version::ONE) }
-
-        // get a hint: first input's outpoint
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 36 OP_EQUALVERIFY
-
-        // get a hint: second input's outpoint
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 0 OP_EQUAL OP_TOALTSTACK
-        OP_SIZE 36 OP_EQUAL OP_FROMALTSTACK OP_BOOLOR OP_VERIFY
-
-        OP_SIZE 0 OP_EQUAL
-        OP_IF
-            OP_DROP
-            OP_PUSHBYTES_5 OP_PUSHBYTES_0 OP_INVALIDOPCODE OP_INVALIDOPCODE OP_INVALIDOPCODE OP_INVALIDOPCODE
-            OP_CAT
-            { tx::Step2InCounterGadget::from_constant(1) }
-            OP_SWAP OP_CAT
-        OP_ELSE
-            OP_TOALTSTACK
-            OP_PUSHBYTES_5 OP_PUSHBYTES_0 OP_INVALIDOPCODE OP_INVALIDOPCODE OP_INVALIDOPCODE OP_INVALIDOPCODE
-            OP_DUP
-            OP_FROMALTSTACK OP_SWAP
-            OP_CAT4
-            { tx::Step2InCounterGadget::from_constant(2) }
-            OP_SWAP OP_CAT
-        OP_ENDIF
-        OP_CAT2
-
-        { tx::Step4OutCounterGadget::from_constant(2) }
-        OP_CAT2
-
-        // current stack body:
-        //   this scriptpubkey, previous counter, previous tx's amount, previous tx's txid
-        //   txid preimage (1-4)
-
-        // get the previous amount
-        2 OP_ROLL
-        OP_CAT2
-
-        // get the script pub key
-        3 OP_ROLL
-        OP_PUSHBYTES_1 OP_PUSHBYTES_34 OP_SWAP
-        OP_CAT3
-
-        { tx::step5_output::Step1AmountGadget::from_constant(&Amount::from_sat(DUST_AMOUNT)) }
-        OP_CAT2
-
-        // push the script hash header
-        OP_PUSHBYTES_2 OP_RETURN OP_PUSHBYTES_8
-
-        3 OP_ROLL
-
-        // extend the actual counter to 4 bytes
-        { CppInt32Gadget::from_positive_bitcoin_integer() }
-
-        // get a hint: the randomizer for previous transaction (4 bytes)
-        OP_DEPTH OP_1SUB OP_ROLL
-        OP_SIZE 4 OP_EQUALVERIFY
-        OP_CAT3
-
-        OP_SHA256
-
-        OP_PUSHBYTES_3 OP_PUSHBYTES_34 OP_PUSHBYTES_0 OP_PUSHBYTES_32
-        OP_SWAP OP_CAT3
-
-        { tx::Step6LockTimeGadget::from_constant_absolute(&LockTime::ZERO) }
-        OP_CAT2
-
-        OP_SHA256
-        OP_SHA256
-
-        OP_EQUAL
+        OP_FROMALTSTACK OP_FROMALTSTACK
+        // [prev_counter, new_counter]
+        { n } OP_SUB OP_EQUAL
     }
 }
 
 #[cfg(test)]
 mod test {
     use crate::counter::{
-        get_script, get_script_counter_plus_one, get_script_pub_key_and_control_block, get_tx,
+        get_script_counter_plus_one, get_script_pub_key_and_control_block, get_tx,
         CounterUpdateInfo, DUST_AMOUNT,
     };
     use crate::treepp::*;
---
+++ src/counter/script_taptree.rs
@@ -0,0 +1,82 @@
+use bitcoin::key::UntweakedPublicKey;
+use bitcoin::taproot::LeafVersion;
+use bitcoin::taproot::TaprootBuilder;
+use bitcoin::taproot::TaprootSpendInfo;
+use bitcoin::{
+    Amount, OutPoint, ScriptBuf, Sequence, TapLeafHash, TapSighashType, Transaction, TxIn, TxOut,
+    Txid, Witness, WitnessProgram,
+};
+use std::str::FromStr;
+
+pub struct ScriptTapTree {
+    spend_info: TaprootSpendInfo,
+    pubkey: ScriptBuf,
+}
+
+impl ScriptTapTree {
+    pub fn new(scripts: Vec<ScriptBuf>) -> Self {
+        let num_scripts = scripts.len();
+        let log2 = (num_scripts as u32).ilog2() as u8;
+        let prev_power_of_2 = 2_usize.pow(log2 as u32);
+
+        // Build the witness program.
+        let secp = bitcoin::secp256k1::Secp256k1::new();
+        let internal_key = UntweakedPublicKey::from(
+            bitcoin::secp256k1::PublicKey::from_str(
+                "0250929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0",
+            )
+            .unwrap(),
+        );
+
+        // add the individual scripts into Taptree (as leaf node)
+        // push the first prev_power_of_2 leaf nodes
+        let mut taproot_builder = TaprootBuilder::new();
+        for i in 0..prev_power_of_2 {
+            taproot_builder = taproot_builder
+                .add_leaf(log2 + 1, scripts[i].clone())
+                .unwrap();
+        }
+        // [None, N1]
+
+        // push the remained leaf nodes except the last one
+        for i in prev_power_of_2..(num_scripts - 1) {
+            let depth = 2 + i - prev_power_of_2;
+            taproot_builder = taproot_builder
+                .add_leaf(depth as u8, scripts[i].clone())
+                .unwrap();
+        }
+        // [None, N1, a_m, a_{m + 1}, a_{n - 2}]
+
+        // push the last leaf node
+        taproot_builder = taproot_builder
+            .add_leaf(
+                (num_scripts - prev_power_of_2) as u8,
+                scripts[num_scripts - 1].clone(),
+            )
+            .unwrap();
+        // [N0]
+        let taproot_spend_info = taproot_builder.finalize(&secp, internal_key).unwrap();
+        let witness_program =
+            WitnessProgram::p2tr(&secp, internal_key, taproot_spend_info.merkle_root());
+
+        let pub_key = ScriptBuf::new_witness_program(&witness_program);
+        Self {
+            spend_info: taproot_spend_info,
+            pubkey: pub_key,
+        }
+    }
+
+    pub fn get_pub_key(self) -> ScriptBuf {
+        self.pubkey
+    }
+
+    pub fn get_control_block(self, script: ScriptBuf) -> Vec<u8> {
+        let mut control_block_bytes = Vec::new();
+        self.spend_info
+            .control_block(&(script, LeafVersion::TapScript))
+            .unwrap()
+            .encode(&mut control_block_bytes)
+            .unwrap();
+        control_block_bytes
+    }
+}
