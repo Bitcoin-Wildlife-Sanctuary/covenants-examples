5b94082755f40c68fd35af83b9af8a7938f3a0ba
Weikeng Chen <w.k@berkeley.edu>
2024-07-11 12:46:55 +0800

    Refactor the `print_address` (#6)

--- src/bin/print_address.rs
+++ src/bin/print_address.rs
@@ -1,11 +1,8 @@
 use bitcoin::hashes::Hash;
 use bitcoin::opcodes::all::{OP_PUSHBYTES_36, OP_RETURN};
 use bitcoin::{Address, Network, ScriptBuf, WScriptHash};
-use bitcoin_scriptexec::utils::scriptint_vec;
-use covenants_examples::common::get_script_pub_key;
+use covenants_examples::common::{get_script_pub_key, CovenantProgram};
 use covenants_examples::counter::CounterProgram;
-use sha2::digest::Update;
-use sha2::{Digest, Sha256};
 
 fn main() {
     let script_pub_key = get_script_pub_key::<CounterProgram>();
@@ -13,14 +10,10 @@ fn main() {
     let program_address =
         Address::from_script(script_pub_key.as_script(), Network::Signet).unwrap();
 
-    let mut bytes = vec![OP_RETURN.to_u8(), OP_PUSHBYTES_36.to_u8()];
-    let counter = 0;
+    let init_state = CounterProgram::new();
+    let hash = CounterProgram::get_hash(&init_state);
 
-    let hash = {
-        let mut sha256 = Sha256::new();
-        Update::update(&mut sha256, &scriptint_vec(counter as i64));
-        sha256.finalize().to_vec()
-    };
+    let mut bytes = vec![OP_RETURN.to_u8(), OP_PUSHBYTES_36.to_u8()];
     bytes.extend_from_slice(&hash);
     bytes.extend_from_slice(&12u32.to_le_bytes());
 
